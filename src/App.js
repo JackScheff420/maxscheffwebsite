66b5eeab3f5d6526494cc12373634b91b42ea896
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2022-11-29 10:20:20 +0100

    button

--- src/App.js
+++ src/App.js
@@ -7,19 +7,9 @@ function App() {
       <header className="App-header">
         
         <p>
-          Hier steht ein neuer text
+          Hier steht ein anderer text
         </p>
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-          <div class="toast-header">
-            <img src="..." class="rounded me-2" alt="..."/>
-            <strong class="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-          </div>
-          <div class="toast-body">
-            Hello, world! This is a toast message.
-          </div>
-        </div>
+        <button type="button" class="btn btn-primary">Primary</button>
         
       </header>
     </div>
