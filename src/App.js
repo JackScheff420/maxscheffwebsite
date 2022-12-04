27d3ce73df23cc800cc43c27421d547e1969ec40
Max Scheff <mxscheffler4@gmail.com>
2022-12-04 15:15:38 +0100

    Website angepasst

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Hier steht ein anderer text
-        </p>
-        <button type="button" class="btn btn-primary">Primary</button>
-        
-      </header>
+    <div>
+
+      <Navbar bg="primary" variant="dark" style={{ color: '#fff' }}>
+        <Navbar.Brand>Maximilian Scheffler Website</Navbar.Brand>
+      </Navbar>
+      <div class="container border">
+        <div class="row">
+          <div class="col-sm">
+            One of three columns
+          </div>
+          <div class="col-sm">
+            One of three columns
+          </div>
+          <div class="col-sm">
+            One of three columns
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
