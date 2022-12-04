756b39d22695a5fa6348c06d2e476c7fab09741c
Max Scheff <mxscheffler4@gmail.com>
2022-12-04 15:39:27 +0100

    einfacher aufbau der website erstellt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
 
 function App() {
-
+  const handleClick = () => {
+    window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
+  };
 
   return (
     <div>
@@ -16,7 +19,11 @@ function App() {
             One of three columns
           </div>
           <div class="col-sm">
-            One of three columns
+            <div>
+              <button type="button" className="btn btn-primary" onClick={handleClick}>
+                Go to Google
+              </button>
+            </div>
           </div>
           <div class="col-sm">
             One of three columns
